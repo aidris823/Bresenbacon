5d43a96083e9e09517d2a312c27b9d3d4ca44926
aidris823 <pokeanand@gmail.com>
2019-02-08 11:01:49 -0500

    oof

---
+++ line_draw.py
@@ -0,0 +1,11 @@
+from display import *
+
+def draw_line(x0,y0,x1,y1,screen,color):
+    float slope = ((y1 - y0) / (x1 - x0))
+    
+    #Octant 1 / 5
+    if (x
+    #Octant 2 / 6
+    #Octant 7 / 3
+    #Octant 8 / 4
+    
